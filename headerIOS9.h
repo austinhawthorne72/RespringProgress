123329710121796e063e64d56be3181896bf51aa
austinhawthorne72 <austinhawthorne72@gmail.com>
2016-09-01 07:51:30 -0500

    Delete headerIOS9.h

--- headerIOS9.h
+++
@@ -1,9 +0,0 @@
- 
-
-@interface SPUISearchViewController : NSObject
-// called when view is being dismissed
-- (void)_didFinishDismissing;
-// clear stuff
-- (void)_clearSearchResults;
-- (void)_searchFieldEditingChanged;
-@end
